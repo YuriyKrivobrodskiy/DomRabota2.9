83ccf8ca7925df830628b13d0c3eaceff64e4adf
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-05-31 19:05:51 +0300

    zadacha 64

--- zadacha64/Program.cs
+++ zadacha64/Program.cs
@@ -1,22 +1,15 @@
-﻿// Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
-// N = 5. -> "5, 4, 3, 2, 1"
-// N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-NaturalToLow(number, count);
-
-
-void NaturalToLow(int n, int count)
-{
-    if (count > n)
-    {
-        return;
-    }
-    else
-    {
-        NaturalToLow(n, count + 1);
-        Console.Write(count + " ");
-    }
-}
+﻿Console.Write("Введите значение M: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+        if (M>N)
+                for (int i = N; i <= M; i++)
+            Console.Write($" {i}");
+           else
+                for (int i = M; i <= N; i++)
+            Console.Write($" {i}");
